18d0602ac1a8d6617a8c19e49228364c949de76e
davidtv2008 <davidtv2008@hotmail.com>
2018-07-11 14:52:25 -0700

    homework2

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+"env": {
+"browser": true
+},
+"extends": "eslint:recommended",
+"rules": {
+"indent": [
+"error",
+2
+],
+"linebreak-style": [
+"error",
+"unix"
+// change to “unix” if on Mac OSX or on Linux
+],
+"quotes": [
+1,
+"single"
+],
+"semi": [
+"error",
+"always"
+]
+}
+};
